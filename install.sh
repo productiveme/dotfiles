5b5a9fe93082e11a7a0cf6dd1767776134815cef
Jaco Swarts <jacoswarts@Jacos-MacBook-Air.local>
2024-04-24 10:10:53 +0200

    Add install script

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+echo "This script will reset your dotfiles and overwrite any custom settings."
+echo "Only run this from within the dotfiles folder. Current folder is $(pwd)."
+echo "Press Ctrl-C to cancel or Enter to continue..."
+read -sr
+
+dotfiles=".alias .bashrc .gitconfig .config/nvim .config/tmux .profile .zshrc"
+#==============
+# Remove old dot flies
+#==============
+for dotfile in $dotfiles; do
+  sudo rm -rf "$HOME"/"$dotfile" > /dev/null 2>&1
+done
+#==============
+# Create symdotfiles in the home folder
+# Allow overriding with files of matching names in the custom-configs dir
+#==============
+SYMLINKS=()
+for dotfile in $dotfiles; do
+  ln -sf "$(pwd)"/"$dotfile" "$HOME"/"$dotfile"
+  SYMLINKS+=("$dotfile")
+done
+
+echo "${SYMLINKS[@]}"
+
+touch $HOME/.private
+
+#==============
+# Set zsh as the default shell
+#==============
+chsh -s /bin/zsh
+
