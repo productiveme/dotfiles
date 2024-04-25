bd7b96be1a230cfa83d9fae30dafcee22ec216a8
Jaco Swarts <jaco@productive.me>
2024-04-25 11:14:59 +0200

    Harpoon tweaks

--- .config/nvim/lua/custom/plugins/harpoon.lua
+++ .config/nvim/lua/custom/plugins/harpoon.lua
@@ -8,30 +8,55 @@ return {
 
     local conf = require("telescope.config").values
     local function toggle_telescope(harpoon_files)
-      local file_paths = {}
-      for _, item in ipairs(harpoon_files.items) do
-        table.insert(file_paths, item.value)
+      local finder = function()
+        local paths = {}
+        for _, item in ipairs(harpoon_files.items) do
+          table.insert(paths, item.value)
+        end
+
+        return require("telescope.finders").new_table({
+          results = paths,
+        })
       end
 
       require("telescope.pickers")
         .new({}, {
           prompt_title = "Harpoon",
-          finder = require("telescope.finders").new_table({
-            results = file_paths,
-          }),
-          previewer = conf.file_previewer({}),
-          sorter = conf.generic_sorter({}),
+          finder = finder(),
+          previewer = false,
+          sorter = require("telescope.config").values.generic_sorter({}),
+          layout_config = {
+            height = 0.4,
+            width = 0.5,
+            prompt_position = "top",
+            preview_cutoff = 120,
+          },
+          attach_mappings = function(prompt_bufnr, map)
+            map("i", "<C-x>", function()
+              local state = require("telescope.actions.state")
+              local selected_entry = state.get_selected_entry()
+              local current_picker = state.get_current_picker(prompt_bufnr)
+
+              table.remove(harpoon_files.items, selected_entry.index)
+              current_picker:refresh(finder())
+            end)
+            return true
+          end,
         })
         :find()
     end
 
     local keymap = vim.keymap
+
     keymap.set("n", "<leader>pa", function()
       harpoon:list():add()
     end, { desc = "[A]dd file to harpoon" })
     keymap.set("n", "<leader>sp", function()
       toggle_telescope(harpoon:list())
-    end, { desc = "Open har[P]oon menu" })
+    end, { desc = "Open har[P]oon menu with Telescope" })
+    keymap.set("n", "<leader>pp", function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end, { desc = "Open har[P]oon menu with Telescope" })
     keymap.set("n", "<leader>p1", function()
       harpoon:list():select(1)
     end, { desc = "Go to harpoon file [1]" })
@@ -50,7 +75,7 @@ return {
     keymap.set("n", "<leader>pn", function()
       harpoon:list():next()
     end, { desc = "[N]ext harpoon file" })
-    keymap.set("n", "<leader>pp", function()
+    keymap.set("n", "<leader>pN", function()
       harpoon:list():prev()
     end, { desc = "[P]revious harpoon file" })
   end,
