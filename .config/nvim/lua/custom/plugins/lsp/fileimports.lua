ba5ca0c0b8610c05b87a762e5747f375d1344e1a
Jaco Home <jaco@Jacos-MacBook-Air.local>
2024-04-29 09:01:28 +0200

    Add fileimports and attempt to add refactoring

---
+++ .config/nvim/lua/custom/plugins/lsp/fileimports.lua
@@ -0,0 +1,10 @@
+return {
+  "kristijanhusak/vim-js-file-import",
+  dependencies = {
+    "ludovicchabant/vim-gutentags",
+  },
+  config = function()
+    vim.g.js_file_import_no_mappings = 1
+    vim.keymap.set("n", "<leader>ci", "<Plug>(JsFileImport)", {})
+  end,
+}
--- .config/nvim/lua/custom/plugins/treesitter.lua
+++ .config/nvim/lua/custom/plugins/treesitter.lua
@@ -1,57 +1,69 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
-  build = ":TSUpdate",
-  dependencies = {
-    "windwp/nvim-ts-autotag",
-  },
-  config = function()
-    -- import nvim-treesitter plugin
-    local treesitter = require("nvim-treesitter.configs")
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    dependencies = {
+      "windwp/nvim-ts-autotag",
+      "nvim-treesitter/nvim-treesitter-refactor",
+    },
+    config = function()
+      -- import nvim-treesitter plugin
+      local treesitter = require("nvim-treesitter.configs")
 
-    -- configure treesitter
-    treesitter.setup({ -- enable syntax highlighting
-      highlight = {
-        enable = true,
-      },
-      -- enable indentation
-      indent = { enable = true },
-      -- enable autotagging (w/ nvim-ts-autotag plugin)
-      autotag = {
-        enable = true,
-      },
-      -- ensure these language parsers are installed
-      ensure_installed = {
-        "json",
-        "javascript",
-        "typescript",
-        "tsx",
-        "yaml",
-        "html",
-        "css",
-        "prisma",
-        "markdown",
-        "markdown_inline",
-        "svelte",
-        "graphql",
-        "bash",
-        "lua",
-        "vim",
-        "dockerfile",
-        "gitignore",
-        "query",
-        "vimdoc",
-        "c",
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<cr>",
-          node_incremental = "<tab>",
-          scope_incremental = false,
-          node_decremental = "<s-tab>",
+      -- configure treesitter
+      treesitter.setup({ -- enable syntax highlighting
+        highlight = {
+          enable = true,
         },
-      },
-    })
-  end,
+        -- enable indentation
+        indent = { enable = true },
+        -- enable autotagging (w/ nvim-ts-autotag plugin)
+        autotag = {
+          enable = true,
+        },
+        -- ensure these language parsers are installed
+        ensure_installed = {
+          "json",
+          "javascript",
+          "typescript",
+          "tsx",
+          "yaml",
+          "html",
+          "css",
+          "prisma",
+          "markdown",
+          "markdown_inline",
+          "svelte",
+          "graphql",
+          "bash",
+          "lua",
+          "vim",
+          "dockerfile",
+          "gitignore",
+          "query",
+          "vimdoc",
+          "c",
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<cr>",
+            node_incremental = "<tab>",
+            scope_incremental = false,
+            node_decremental = "<s-tab>",
+          },
+          refactor = {
+            smart_rename = {
+              enable = true,
+              -- Assign keymaps to false to disable them, e.g. `smart_rename = false`.
+              keymaps = {
+                smart_rename = "grr",
+              },
+            },
+          },
+        },
+      })
+    end,
+  },
 }
