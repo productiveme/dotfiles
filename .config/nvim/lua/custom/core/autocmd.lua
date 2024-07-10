87ced1a71218b9c1e100bbdae7a564ff8e2e22b9
Jaco Swarts <jaco@productive.me>
2024-07-10 09:48:44 +0200

    Add support for GBrowse (open github from code)

--- .config/nvim/lua/custom/core/autocmd.lua
+++ .config/nvim/lua/custom/core/autocmd.lua
@@ -34,3 +34,8 @@ end
 
 -- GitHub Issues
 vim.keymap.set("n", "<leader>gi", CreateGHIssue, { silent = true, desc = "Create [G]itHub [I]ssue" })
+
+-- Define own Browse for GBrowse
+vim.api.nvim_create_user_command("Browse", function(opts)
+  vim.fn.system({ "open", opts.fargs[1] })
+end, { nargs = 1 })
--- .zshrc
+++ .zshrc
@@ -17,3 +17,4 @@ source "$HOME"/.private
 #THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
 export SDKMAN_DIR="$HOME/.sdkman"
 [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+export PATH=$HOME/.meteor:$PATH
