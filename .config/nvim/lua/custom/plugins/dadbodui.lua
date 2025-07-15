63263188122b3ef65aba70ee2d4e7db7e865d410
Jaco Swarts <jaco@productive.me>
2025-07-15 11:41:57 +0200

    Add quicker

--- .config/nvim/lua/custom/plugins/dadbodui.lua
+++ .config/nvim/lua/custom/plugins/dadbodui.lua
@@ -2,7 +2,7 @@ return {
   "kristijanhusak/vim-dadbod-ui",
   dependencies = {
     { "tpope/vim-dadbod", lazy = true },
-    { "kristijanhusak/vim-dadbod-completion", ft = { "mongodb" }, lazy = true },
+    -- { "kristijanhusak/vim-dadbod-completion", ft = { "mongodb" }, lazy = true },
   },
   cmd = {
     "DBUI",
